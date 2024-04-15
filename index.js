db63e1628b6705174410ebbb5aea95068d84c138
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-04-15 16:25:15 -0400

    fixed issue with traingle

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const questions = require('./lib/questions.js');
 const setshape = require ('./lib/setshape.js')
-const fileName = "./examples/logo.svg"
+const fileName = "./dist/logo.svg"
 
 // function to create new svg file using inquirer response and file system
 
 function createLogo(response){
     const svg = setshape (response)
-    fs.writeFile(fileName, svg, () => console.log ('Generated logo,svg'))
+    fs.writeFile(fileName, svg, () => console.log ('Generated logo.svg'))
 }
 
 // initialize, ask questions then createLogo using responses, catch any errors
